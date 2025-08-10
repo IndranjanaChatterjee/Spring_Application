4a2e910fd2dc67387785c9742d1b1fe32dd9bd5f
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2025-08-10 12:07:17 +0530

    Feat:Layer files

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -1,7 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Department;
+import com.example.demo.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
+    /* Spring wires the object already created with this referred object */
+    @Autowired
+    private DepartmentService departmentService;
+    /* @RequestBody is used for converting the data to the json format */
+    @PostMapping("/departments")
+    public Department saveDepartment(@RequestBody Department department)
+    {
+      return departmentService.saveDepartment(department);
+    }
 }
--- src/main/java/com/example/demo/service/DepartmentService.java
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -1,4 +1,8 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Department;
+
 public interface DepartmentService {
+
+    public Department saveDepartment(Department department);
 }
