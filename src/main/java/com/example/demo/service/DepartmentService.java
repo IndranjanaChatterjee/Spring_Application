1f658b6b476d668d5726d5477b8985251c84b704
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2025-08-10 19:27:47 +0530

    Feat:Fetching data

--- src/main/java/com/example/demo/service/DepartmentService.java
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -2,7 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
 
     public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
 }
--- src/main/java/com/example/demo/service/DepartmentServiceImplementation.java
+++ src/main/java/com/example/demo/service/DepartmentServiceImplementation.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DepartmentServiceImplementation implements DepartmentService{
     @Autowired
@@ -13,5 +15,10 @@ public class DepartmentServiceImplementation implements DepartmentService{
     public Department saveDepartment(Department department)
     {
         return departmentRepository.save(department);
+    }
+
+    @Override
+    public List<Department> fetchDepartmentList() {
+        return departmentRepository.findAll();
     }
 }
