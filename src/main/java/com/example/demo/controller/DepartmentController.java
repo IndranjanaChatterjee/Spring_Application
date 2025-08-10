5f4bd1ad3ab07a42f5fd350d5c918a66e0073b06
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2025-08-10 19:42:14 +0530

    Feat:Fetching data by ID

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -3,10 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.Department;
 import com.example.demo.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,10 @@ public class DepartmentController {
     public List<Department> fetchDepartmentList()
     {
         return departmentService.fetchDepartmentList();
+    }
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId)
+    {
+        return departmentService.fetchDepartmentById(departmentId);
     }
 }
