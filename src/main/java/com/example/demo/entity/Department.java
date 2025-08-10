3165fb547c207528d59d4d80958634362ab9f1fc
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2025-08-10 11:33:49 +0530

    Feat:Layer files

---
+++ src/main/java/com/example/demo/entity/Department.java
@@ -0,0 +1,65 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+/* We have to add the annotation Entity to the class so that it can interact with the H2 database */
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentCode;
+    private String departmentAddress;
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentCode() {
+        return departmentCode;
+    }
+
+    public void setDepartmentCode(String departmentCode) {
+        this.departmentCode = departmentCode;
+    }
+
+    public String getDepartmentAddress() {
+        return departmentAddress;
+    }
+
+    public void setDepartmentAddress(String departmentAddress) {
+        this.departmentAddress = departmentAddress;
+    }
+
+    public Department(Long departmentId,String departmentAddress,String departmentCode,String departmentName)
+    {
+        this.departmentId=departmentId;
+        this.departmentCode=departmentCode;
+        this.departmentAddress=departmentAddress;
+        this.departmentName=departmentName;
+    }
+
+    public Department(){
+    }
+
+    @Override
+    public String toString(){
+        return "Department{"+departmentName+" "+departmentAddress+" "+departmentCode+"of id "+departmentId+"}";
+    }
+}
