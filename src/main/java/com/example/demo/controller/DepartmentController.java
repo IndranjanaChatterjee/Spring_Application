3199b49a626d8b20669a43d1e09d4e96199335ea
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2025-08-11 09:47:38 +0530

    Feat:Loggers and Lombok plugin

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -3,25 +3,36 @@ package com.example.demo.controller;
 import com.example.demo.entity.Department;
 import com.example.demo.service.DepartmentService;
 import jakarta.validation.Valid;
+import org.apache.juli.logging.Log;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
+
 
 @RestController
 public class DepartmentController {
     /* Spring wires the object already created with this referred object */
     @Autowired
     private DepartmentService departmentService;
+
+
+    private final Logger logger= LoggerFactory.getLogger(DepartmentController.class);
     /* @RequestBody is used for converting the data to the json format */
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department)
     {
-      return departmentService.saveDepartment(department);
+        logger.info("Inside save department of department controller");
+        return departmentService.saveDepartment(department);
     }
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList()
     {
+        logger.info("Inside fetch department of department controller");
         return departmentService.fetchDepartmentList();
     }
     @GetMapping("/departments/{id}")
--- src/main/java/com/example/demo/entity/Department.java
+++ src/main/java/com/example/demo/entity/Department.java
@@ -5,9 +5,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 /* We have to add the annotation Entity to the class so that it can interact with the H2 database */
+
+@Data
+/* This consist of all the getter, setters etc */
+@NoArgsConstructor
+/* No Argumemt Constructor */
+
+@AllArgsConstructor
+@Builder
+/*Adds the builder pattern */
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -18,51 +31,5 @@ public class Department {
     private String departmentCode;
     private String departmentAddress;
 
-    public Long getDepartmentId() {
-        return departmentId;
-    }
 
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public Department(Long departmentId,String departmentAddress,String departmentCode,String departmentName)
-    {
-        this.departmentId=departmentId;
-        this.departmentCode=departmentCode;
-        this.departmentAddress=departmentAddress;
-        this.departmentName=departmentName;
-    }
-
-    public Department(){
-    }
-
-    @Override
-    public String toString(){
-        return "Department{"+departmentName+" "+departmentAddress+" "+departmentCode+"of id "+departmentId+"}";
-    }
 }
