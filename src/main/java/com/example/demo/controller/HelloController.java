e684ad20a82325947a5be7b118f50f0ec5593b35
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2025-08-11 12:45:46 +0530

    Feat:Config file configuration

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String helloWorld()
     {
-        return "Welcome World Computer";
+        return welcomeMessage;
     }
 }
